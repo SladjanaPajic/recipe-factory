6efa407b4551d1649c004da831a49d4029d102ec
Sladjana <sladjana_pajic@hotmail.com>
2018-06-02 17:17:21 +0200

    header & footer added

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+      
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
